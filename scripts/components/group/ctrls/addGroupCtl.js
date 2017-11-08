9a5c807413d6c60273afc205af771fedf8fb9b8e
Niuxinyi <15754326113@163.com>
2017-11-08 15:02:07 +0800

    修改新增、编辑

--- scripts/components/group/ctrls/addGroupCtl.js
+++ scripts/components/group/ctrls/addGroupCtl.js
@@ -5,10 +5,63 @@ angular.module('app').controller('addGroupCtrl', ['$window', '$scope', '$timeout
     function ($window, $scope, $timeout, NgTableParams, dsFcc, appPath) {
         // 初始化表格;
         $scope.cityName = App.Temp.infoToGroupData.cityName;
-        $scope.saveGroup = function () {
-            if (!$scope.principle) {
-                swal('提示', '请输入限制信息', 'warning');
+        $scope.row = [
+            {
+                time: '',
+                target: '',
+                range: '',
+                strategy: ''
+            }
+        ];
+        $scope.addItem = function () {
+            $scope.row.push({
+                time: '',
+                target: '',
+                range: '',
+                strategy: ''
+            });
+        };
+        $scope.deleteItem = function (index) {
+            if ($scope.row.length === 1) {
                 return;
+            }
+            $scope.row.splice(index, 1);
+        };
+        $scope.limit = function (event, index, name) {
+            if (event.keyCode === 220) {
+                swal('提示', '不能输入"|"', 'warning');
+                $scope.row[index][name] = $scope.row[index][name].substring(0, $scope.row[index][name].length - 1);
+            }
+        };
+        $scope.saveGroup = function () {
+            var principle = '';
+            for (var i = 0; i < $scope.row.length; i++) {
+                if (!$scope.row[i].time) {
+                    swal('提示', '请输入限制时间', 'warning');
+                    return;
+                }
+                if (!$scope.row[i].target) {
+                    swal('提示', '请输入限行对象', 'warning');
+                    return;
+                }
+                if (!$scope.row[i].range) {
+                    swal('提示', '请输入限行范围', 'warning');
+                    return;
+                }
+                if (!$scope.row[i].strategy) {
+                    swal('提示', '请输入限行策略', 'warning');
+                    return;
+                }
+                var str = '限行时间:' + $scope.row[i].time + '|限行对象:' + $scope.row[i].target + '|限行范围:' + $scope.row[i].range + '|限行策略:' + $scope.row[i].strategy;
+                if (i + 1 === $scope.row.length) {
+                    principle += str;
+                } else {
+                    principle = principle + str + '||';
+                }
+                if (principle.length >= 600) {
+                    swal('提示', '超出最大字段长度', 'warning');
+                    return;
+                }
             }
             var params = {
                 command: 'CREATE',
@@ -17,7 +70,7 @@ angular.module('app').controller('addGroupCtrl', ['$window', '$scope', '$timeout
                     infoIntelId: App.Temp.infoToGroupData.infoId,
                     adAdmin: App.Temp.infoToGroupData.cityId,
                     groupType: 1,
-                    principle: $scope.principle,
+                    principle: principle,
                     condition: App.Temp.infoToGroupData.condition
                 }
             };
--- scripts/components/group/ctrls/editGroupCtl.js
+++ scripts/components/group/ctrls/editGroupCtl.js
@@ -22,10 +22,55 @@ angular.module('app').controller('editGroupCtrl', ['$window', '$scope', '$timeou
                 name: '已制作'
             }
         ];
-        $scope.saveGroup = function () {
-            if (!$scope.groupData.principle) {
-                swal('提示', '请输入限制信息', 'warning');
+        $scope.addItem = function () {
+            $scope.row.push({
+                time: '',
+                target: '',
+                range: '',
+                strategy: ''
+            });
+        };
+        $scope.deleteItem = function (index) {
+            if ($scope.row.length === 1) {
                 return;
+            }
+            $scope.row.splice(index, 1);
+        };
+        $scope.limit = function (event, index, name) {
+            if (event.keyCode === 220) {
+                swal('提示', '不能输入"|"', 'warning');
+                $scope.row[index][name] = $scope.row[index][name].substring(0, $scope.row[index][name].length - 1);
+            }
+        };
+        $scope.saveGroup = function () {
+            var principle = '';
+            for (var i = 0; i < $scope.row.length; i++) {
+                if (!$scope.row[i].time) {
+                    swal('提示', '请输入限制时间', 'warning');
+                    return;
+                }
+                if (!$scope.row[i].target) {
+                    swal('提示', '请输入限行对象', 'warning');
+                    return;
+                }
+                if (!$scope.row[i].range) {
+                    swal('提示', '请输入限行范围', 'warning');
+                    return;
+                }
+                if (!$scope.row[i].strategy) {
+                    swal('提示', '请输入限行策略', 'warning');
+                    return;
+                }
+                var str = '限行时间:' + $scope.row[i].time + '|限行对象:' + $scope.row[i].target + '|限行范围:' + $scope.row[i].range + '|限行策略:' + $scope.row[i].strategy;
+                if (i + 1 === $scope.row.length) {
+                    principle += str;
+                } else {
+                    principle = principle + str + '||';
+                }
+                if (principle.length >= 600) {
+                    swal('提示', '超出最大字段长度', 'warning');
+                    return;
+                }
             }
             var params = {
                 command: 'UPDATE',
@@ -33,7 +78,7 @@ angular.module('app').controller('editGroupCtrl', ['$window', '$scope', '$timeou
                 objId: $scope.groupData.groupId,
                 data: {
                     groupType: $scope.groupData.groupType,
-                    principle: $scope.groupData.principle,
+                    principle: principle,
                     objStatus: 'UPDATE'
                 }
             };
@@ -44,6 +89,17 @@ angular.module('app').controller('editGroupCtrl', ['$window', '$scope', '$timeou
         };
         var initialize = function (event, data) {
             $scope.groupData = Object.assign({}, data.data);
+            $scope.row = [];
+            var doubleRow = $scope.groupData.principle.split('||');
+            for (var i = 0; i < doubleRow.length; i++) {
+                var arr = doubleRow[i].split('|');
+                $scope.row.push({
+                    time: arr[0].substring(5),
+                    target: arr[1].substring(5),
+                    range: arr[2].substring(5),
+                    strategy: arr[3].substring(5)
+                });
+            }
         };
 
         var unbindHandler = $scope.$on('ReloadData-editGroup', initialize);
